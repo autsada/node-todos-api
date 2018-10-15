a90a57826ba9fbc135e22eae693a37fc4fa6d521
Autsada <autsada.t@gmail.com>
2018-10-15 18:26:02 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
         }, e => {
             res.status(400).send(e);
         } );
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.send(404).send();
+            }
+            res.send({ todo });
+        }, e => {
+            res.status(400).send();
+        });
 });
 
 app.listen(3000, () => {
