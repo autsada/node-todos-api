5629bf0c9e646186e51ae4e173e1e563da5963cd
Autsada <autsada.t@gmail.com>
2018-10-20 12:03:24 +0700

    Update heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,27 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // tell mongoose to use built-in promise library
 
 //connect mongoose to database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://autsada:ann@1ong@2@ds163162.mlab.com:63162/node-todo-api'
+};
+
+mongoose
+  .connect(
+    db.localhost,
+    { useMongoClient: true }
+  )
+  .then(
+    () => {},
+    err => {
+      mongoose.connect(
+        db.mlab,
+        { useMongoClient: true }
+      );
+    }
+  );
 
 // mongoose
 //   .connect(
