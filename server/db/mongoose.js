0cc271d03da1229a167141e60e8d922b85786a97
Autsada <autsada.t@gmail.com>
2018-10-20 11:49:13 +0700

    Update heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // tell mongoose to use built-in promise library
 
-// connect mongoose to database
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+//connect mongoose to database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
-mongoose
-  .connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
-    { useMongoClient: true }
-  )
-  .then(() => {});
+// mongoose
+//   .connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+//     { useMongoClient: true }
+//   )
+//   .then(() => {});
 module.exports = { mongoose };
