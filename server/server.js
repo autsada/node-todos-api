b83bc4d8609f8257fef5d3cab3875424ead2a109
Autsada <autsada.t@gmail.com>
2018-10-21 13:34:45 +0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -64,13 +65,40 @@ app.delete('/todos/:id', (req, res) => {
     return res.sendStatus(404).send();
   }
 
-  Todo.findByIdAndDelete(id).then(
-    todo => {
+  Todo.findByIdAndDelete(id)
+    .then(todo => {
       if (!todo) {
         return res.sendStatus(404).send();
       }
       res.send({ todo });
-    }).catch (error => res.sendStatus(400).send());
+    })
+    .catch(error => res.sendStatus(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']); // Pick 'text' and 'completed' properties from todo object and only allow user to update these two.
+
+  if (!ObjectID.isValid(id)) {
+    return res.sendStatus(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime(); // getTime() return timestamp (base time is midnight 1 Jan 1970) -> millisecond + or -
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+        if (!todo) {
+            return res.sendStatus(404).send();
+        }
+
+        res.send({ todo });
+    }).catch (error => res.sendStatus(400).send())
+
 });
 
 app.listen(port, () => {
